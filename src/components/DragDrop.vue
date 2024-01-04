e18bba547d953131dbe1d046fb95076a10643ec5
fatihylm <119489180+fatihylm@users.noreply.github.com>
2024-01-04 18:46:28 +0100

    Erstellen der Website
    Website für das umschreiben der CSV Files für die Visualisierung in Grafana

---
+++ src/components/DragDrop.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="header">
+      <h1>Click on the button to upload your file</h1>
+    </div> 
+    <button @click="openFileInput">Upload Files</button>
+    <div class="main">
+      <label for="file-input" id="dropzone" @click="openFileInput">
+      </label>
+      <input ref="fileInput" id="file-input" type="file" @change="handleFileChange" style="display: none;" single />
+        <div class="file-list">
+          <ul v-if="files.length > 0">
+            <li v-for="(file, index) in files" :key="index">
+              {{ file.name }}
+            </li>
+          </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      files: [],
+    };
+  },
+  methods: {
+    openFileInput() {
+      this.$refs.fileInput.click();
+    },
+    handleFileChange(event) {
+      this.files = Array.from(event.target.files);
+    },
+  },
+};
+</script>
+
+<style>
+@import '/src/css/DragDropStyle.css';
+</style>
---
+++ src/components/LogoImage.vue
@@ -0,0 +1,24 @@
+<!-- ExampleComponent.vue -->
+
+<template>
+    <div>
+      <img :src="htldornbirn" alt="Logo" />
+    </div>
+  </template>
+  
+  <script>
+  import htldornbirn from '/src/images/htldornbirn.png'
+
+  export default {
+    data() {
+      return {
+        htldornbirn:htldornbirn
+      };
+    }
+  };
+  </script>
+  
+  <style>
+    @import '/src/css/LogoImageStyle.css';
+ </style>
+  
